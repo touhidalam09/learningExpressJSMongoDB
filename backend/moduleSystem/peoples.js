2f1c297a8c32bcb3267a874b7632d12cdfd126fb
touhidalam09 <touhidalam649@gmail.com>
2022-06-30 23:54:46 +0600

    node module system

---
+++ backend/moduleSystem/peoples.js
@@ -0,0 +1,5 @@
+const peoples = ['Touhid', 'alam', 'rifat'];
+
+console.log('Peoples in same file: ', peoples);
+
+module.exports = peoples;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const _ = require('lodash');
+const peoples = require('./backend/moduleSystem/peoples');
 
-console.log(_.last([1, 2, 3, 4, 5]));
-
-console.log('Hello');
-console.log('Setup');
+console.log('Index: ', peoples);
+console.log('Last Name: ', _.last(peoples));
